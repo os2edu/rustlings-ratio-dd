39f9ce310306efcc324aad5343905e289f0c5f16
ratio <ratio_dd@163.com>
2022-12-31 22:49:42 +0800

    finish 40exercises

--- exercises/enums/README.md
+++ exercises/enums/README.md
@@ -2,7 +2,7 @@
 
 Rust allows you to define types called "enums" which enumerate possible values.
 Enums are a feature in many languages, but their capabilities differ in each language. Rust’s enums are most similar to algebraic data types in functional languages, such as F#, OCaml, and Haskell.
-Useful in combination with enums is Rust's "pattern matching" facility, which makes it easy to run different code for different values of an enumeration.
+Useful in combination with enums is Rust's =="pattern matching"== facility, which makes it easy to run different code for different values of an enumeration.
 
 ## Further information
 
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,28 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+use std::fs::read_to_string;
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    
+    let mut s = input.to_string();
+    s = s.trim_end().to_string();
+    s = s.trim_start().to_string();
+    s
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut s = input.to_string();
+    s.push_str(" world!");
+    s
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let s = input.to_string();
+    s.replace("cars", "balloons")
 }
 
 #[cfg(test)]
